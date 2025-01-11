08a9c2c7bb916c06b1eb909b8b4c45af88002ee6
Buchichacho <80576877+Buchichacho@users.noreply.github.com>
2025-01-11 16:27:36 +0100

    Delete my_backend_project directory

--- my_backend_project/app/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# URL базы данных
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://myuser:mypassword@localhost/mydatabase")
-
-# Конфигурация для JWT
-SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")  # Лучше использовать переменные окружения для безопасности
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
